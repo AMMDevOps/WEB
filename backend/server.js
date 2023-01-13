317746df9962c9f284420905781ae5b2ea2197f2
hrustinszkiadam <hrustinszkiadam@gmail.com>
2023-01-13 10:28:28 +0100

    Co-authored-by: Preisler <Preisler25@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,58 @@
-    //\\
-   //  \\
-  //====\\
- // APP  \\
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+
 app.set('view engine', 'ejs');
-app.listen(3000, () => {console.log('Server started on port 3000');});
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
 
-    //\\
-   //  \\
-  //====\\
- // FUNC \\
- let functions = require('./functions');
+let functions = require('./modules/functions');
 
-    //\\
-   //  \\
-  //====\\
- // BODY \\
-let bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+const { Client } = require('pg');
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'amm',
+    user: 'postgres',
+    password: 'admin'
+});
+client.connect();
 
-    //\\
-   //  \\
-  //====\\
- // PATH \\
-let path = require('path');
 
-    //\\
-   //  \\
-  //====\\
- //  DB  \\
- const { Client } = require('pg');
- const client = new Client({host: 'localhost',port: 5432,database: 'amm',user: 'postgres',password: 'admin'});
- client.connect();
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
 
-    //\\
-   //  \\
-  //====\\
- // CRYP \\
-const bcrypt = require('bcrypt');
+app.get('/js', (req, res) => {
+    res.sendFile('Scripts/main.js');
+});
 
-    //\\
-   //  \\
-  //====\\
- // GET  \\
-app.get('/', (req, res) => {res.render(path.join(__dirname, 'public', 'index.ejs'), { name: 'Kyle' });});
-app.get('/amm', (req, res) => {res.render(path.join(__dirname, 'public', 'main.ejs'));});
+app.get('/main', (req, res) => {
+    res.render('main.ejs', { name: req.session.username });
+});
 
-    //\\
-   //  \\
-  //====\\
- // POST \\
 app.post('/login', async(req, res) => {
     let status = await functions.checkUser(req.body, client)
     console.log(status);
     if(status == true){
-        res.redirect('/amm')
-    }else {
-        res.redirect('/?error=1');
+        req.session.username = req.body.username;
+        res.end(req.session.username);
+        res.redirect(`/main`);
+    } else {
+        res.redirect('/');
     }
 });
+
 app.post('/register', (req, res) => {
     functions.genUser(req.body, client);
     res.redirect('/'); 
 });
+
+app.listen(3000, () => {console.log('Server started on port 3000');});
