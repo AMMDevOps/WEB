9a890bc2a8f83c951138ae09024ef00f7cbf5a68
Preisler25 <preisler.andras@gmail.com>
2023-01-25 15:28:28 +0100

    MAIN

---
+++ backend/arduino/test.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+String stra;
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2); 
+}
+
+void loop() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  stra = Serial.readString();
+  lcd.print(stra);
+  Serial.println(stra);
+  delay(1000);
+}
