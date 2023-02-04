9a1d39cdbc77a6dfa1916960c14968e8f84213cb
Preisler <preisler.andras@gmail.com>
2023-02-04 12:25:51 +0100

    jwt baszakodásos

--- backend/modules/auth.js
+++ backend/modules/auth.js
@@ -7,6 +7,10 @@ function genToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
 }
 
+function genRef(user) {
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '20min' });
+}
+
 
 function loginUser(data) {
     const username = data.username;
@@ -14,11 +18,41 @@ function loginUser(data) {
 
     const accestoken = genToken(user);
     
-    const reftoken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    const reftoken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
     let sql = `UPDATE users SET auth = '${reftoken}' WHERE username = '${username}'`;
     db.pls(sql)
 
     return accestoken;
+}
+
+function findRef(token) {
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        console.log(payload);
+        let sql = `SELECT * FROM users WHERE username = '${payload.name}'`;
+        db.pls(sql, (err, result) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            if (result.rows.length > 0) {
+                let user = result.rows[0];
+                let reftoken = user.auth;
+                let data = testRef(reftoken);
+                return data;
+            }
+        });
+}
+
+function testRef(token) {
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        let accestoken = genToken(user);
+        let reftoken = genRef(user);
+        let sql = `UPDATE users SET auth = '${reftoken}' WHERE username = '${user.name}'`;
+        db.pls(sql);
+        return {authtoken: accestoken, user: user.name};
+    })     
 }
 
 
@@ -28,10 +62,17 @@ function check(req, res, next) {
     if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        console.log(payload);
         if (err) {
-            console.log("user", user);
-            console.log(err);
-            return res.sendStatus(403);
+
+            let data = findRef(payload);
+            if (data == null) {
+                return res.sendStatus(403);
+            }
+            res.cookie('authtoken', data.authtoken);
+            req.user = data.user;
+            next();
         }
         req.user = user;
         next();
