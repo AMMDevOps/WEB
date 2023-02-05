97523b081a2b707f16eabdba2250771d66f67df1
Preisler <preisler.andras@gmail.com>
2023-02-05 14:46:03 +0100

    websocket setup

--- backend/public/Scripts/main.js
+++ backend/public/Scripts/main.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+const socket = io('http://192.168.1.199:8080');
+
+socket.on('connect', () => {
+    console.log('connected');
+});
+
+socket.on('message', (msg) => {
+    console.log(msg);
+});
