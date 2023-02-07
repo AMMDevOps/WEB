cd4a4372df2dea9329ebaa944eb3c3ee5caaa980
Preisler <preisler.andras@gmail.com>
2023-02-07 21:04:29 +0100

    clearing some shit up

--- backend/server.sql
+++ backend/server.sql
@@ -2,4 +2,5 @@ CREATE DATABASE amm;
 \C amm
 CREATE TABLE users (id SERIAL PRIMARY KEY,username VARCHAR(16) NOT NULL,password VARCHAR(16) NOT NULL, email VARCHAR(30) NOT NULL, auth Int, socketid VARCHAR(25));
 CREATE TABLE room (id SERIAL PRIMARY KEY, useroneid INT, usertwoid INT);
+Drop table message;
 CREATE TABLE message (roomid INT, userid INT, message VARCHAR(300), page INT, time TIMESTAMP);
