ddfe338f2e0d2e83bb952d4b7d468ef69154f3df
Preisler <preisler.andras@gmail.com>
2023-02-08 09:27:48 +0100

    working page system

--- backend/modules/functions.js
+++ backend/modules/functions.js
@@ -102,10 +102,13 @@ let getChat = async (room_id) => {
     let sql = `SELECT * FROM message WHERE roomid = ${room_id}`;
     let data = await db.pls(sql);
     let messages = data.rows.reverse();
+    let page = messages[0].page;
     let messages_formated = [];
     for(let i = 0; i < messages.length && i < 20; i++){
+        if (messages[i].page != page) {}else {
         let user = await getUserName(messages[i].userid);
-        messages_formated.push({id: messages[i].id, user: user, message: messages[i].message, time: messages[i].time});
+        messages_formated.push({id: messages[i].id, user: user, message: messages[i].message, time: messages[i].time, page: messages[i].page});
+        }
     }
     return messages_formated.reverse();
 }
